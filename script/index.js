f1080c462d56adc90866b7ef46a909644bb69db8
Lucas Txai <txaifonceca@gmail.com>
2022-09-14 20:02:49 -0300

    Fiz as requisições POST para o endpoint ficticio

--- script/index.js
+++ script/index.js
@@ -13,5 +13,69 @@ const zipCode = document.getElementById('zip-code')
 companyForm.addEventListener('submit', (e) => {
     e.preventDefault()
 
-    console.log(`O nome da empresa é: ${businessName.value}`)
+    sendCompanyData()
+
+    alert("Formulário enviado com sucesso")
+
 })
+
+function sendCompanyData() {
+    const companyData = new FormData(companyForm)
+    axios.post(`https://httpbin.org/post`, companyData)
+        .then((res) => {
+            console.log(res.data.form)
+        }).catch((err) => {
+            console.log(err)
+        })
+}
+
+const responsibleForm = document.getElementById('responsible-data')
+
+const financialName = document.getElementById('financial-name')
+const financialEmail = document.getElementById('financial-email')
+const financialPhone = document.getElementById('financial-phone')
+
+responsibleForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    alert("Formulário enviado com sucesso")
+})
+
+function sendResponsibleData() {
+    const responsibleData = new FormData(responsibleForm)
+
+    axios.post(`https://httpin.org/post`, responsibleData)
+        .then((res) => {
+            console.log(res.data.form)
+        }).catch((err) => {
+            console.log(err)
+        })
+}
+
+const garageForm = document.getElementById('garage-data')
+
+const garageCode = document.getElementById('garage-code')
+const timeDayInitial = document.getElementById('time-day-initial')
+const timeDayFinal = document.getElementById('time-day-final')
+const qtyGuest = document.getElementById('qty-guest')
+const eventDay = document.getElementById('event-day')
+
+function sendGarageData() {
+    const garageData = new FormData(garageForm)
+
+    axios.post(`https://httpin.org/post`, garageData)
+        .then((res) => {
+            console.log(res.data.form)
+        }).catch((err) => {
+            console.log(err)
+        })
+}
+
+/*
+TODO
+1 - reCAPTCHA
+2 - separar funcoes em arquivos diferentes
+3 - validações
+4 - tratar erros
+5 - arrumar estilização
+*/
