0fa68163ef9370ad4f7d4bdcde911db40fec9c89
Lucas Txai <txaifonceca@gmail.com>
2022-09-17 13:18:54 -0300

    adicionei os tipos de erro

--- script/sendCompanyData.js
+++ script/sendCompanyData.js
@@ -1,24 +1,4 @@
-import { eventDay, garageCode, qtyGuest, timeDayFinal, timeDayInitial, financialEmail, financialName, financialPhone, address, businessCnpj, businessEmail, businessName, businessPhone, city, country, state, zipCode } from "./dadosForm.js";
-
 export const companyForm = document.getElementById('company-data')
-
-businessName;
-businessCnpj;
-businessEmail;
-businessPhone;
-address;
-city;
-country;
-state;
-zipCode;
-financialEmail;
-financialName;
-financialPhone;
-garageCode;
-timeDayInitial;
-timeDayFinal;
-qtyGuest;
-eventDay;
 
 export function sendCompanyData() {
     const companyData = new FormData(companyForm)
